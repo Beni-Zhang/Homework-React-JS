aa8ed5958e5bbf5054823784edf277f987532f5d
Beny <benizhang1@gmail.com>
2023-11-10 23:28:42 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,13 +1,33 @@
-
-import * as React from 'react';
+import React, { useState } from 'react';
+import { GameProvider } from './GameContext';
+import { useGame } from './GameContext';
 
 function Board() {
-  const squares = Array(9).fill(null);
-  function selectSquare(square) {
+  const [squares, setSquares] = useState(Array(9).fill(null));
+  const [nextValue, setNextValue] = useState('X');
+  const [winner, setWinner] = useState(null);
 
+  function selectSquare(square) {
+    if (squares[square] || winner) {
+      return;
+    }
+
+    const newSquares = [...squares];
+    newSquares[square] = nextValue;
+    setSquares(newSquares);
+
+    const newWinner = calculateWinner(newSquares);
+    if (newWinner) {
+      setWinner(newWinner);
+    } else {
+      setNextValue(calculateNextValue(newSquares));
+    }
   }
 
   function restart() {
+    setSquares(Array(9).fill(null));
+    setNextValue('X');
+    setWinner(null);
   }
 
   function renderSquare(i) {
@@ -20,54 +40,27 @@ function Board() {
 
   return (
     <div>
-      <div >STATUS</div>
-      <div >
+      <div>STATUS: {calculateStatus(winner, squares, nextValue)}</div>
+      <div>
         {renderSquare(0)}
         {renderSquare(1)}
         {renderSquare(2)}
       </div>
-      <div >
+      <div>
         {renderSquare(3)}
         {renderSquare(4)}
         {renderSquare(5)}
       </div>
-      <div >
+      <div>
         {renderSquare(6)}
         {renderSquare(7)}
         {renderSquare(8)}
       </div>
-      <button onClick={restart}>
-        restart
-      </button>
+      <button onClick={restart}>Restart</button>
     </div>
   );
 }
 
-function Game() {
-  return (
-    <div >
-      <div >
-        <Board />
-      </div>
-    </div>
-  );
-}
-
-// eslint-disable-next-line no-unused-vars
-function calculateStatus(winner, squares, nextValue) {
-  return winner
-    ? `Winner: ${winner}`
-    : squares.every(Boolean)
-      ? `Scratch: Cat's game`
-      : `Next player: ${nextValue}`;
-}
-
-// eslint-disable-next-line no-unused-vars
-function calculateNextValue(squares) {
-  return squares.filter(Boolean).length % 2 === 0 ? 'X' : 'O';
-}
-
-// eslint-disable-next-line no-unused-vars
 function calculateWinner(squares) {
   const lines = [
     [0, 1, 2],
@@ -79,17 +72,60 @@ function calculateWinner(squares) {
     [0, 4, 8],
     [2, 4, 6],
   ];
+
   for (let i = 0; i < lines.length; i++) {
     const [a, b, c] = lines[i];
     if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
       return squares[a];
     }
   }
+
   return null;
 }
 
+function calculateNextValue(squares) {
+  const xCount = squares.filter((square) => square === 'X').length;
+  const oCount = squares.filter((square) => square === 'O').length;
+  return xCount <= oCount ? 'X' : 'O';
+}
+
+function calculateStatus(winner, squares, nextValue) {
+  if (winner) {
+    return `Winner: ${winner}`;
+  } else if (squares.every((square) => square)) {
+    return `Scratch: Cat's game`;
+  } else {
+    return `Next player: ${nextValue}`;
+  }
+}
+
+export { calculateWinner, calculateNextValue, calculateStatus };
+
+function Game() {
+  const { setSquares, setNextValue, setWinner } = useGame();
+
+  const restart = () => {
+    setSquares(Array(9).fill(null));
+    setNextValue('X');
+    setWinner(null);
+  };
+
+  return (
+    <div>
+      <div>
+        <Board />
+      </div>
+      <button onClick={restart}>Restart</button>
+    </div>
+  );
+}
+
 function App() {
-  return <Game />;
+  return (
+    <GameProvider>
+      <Game />
+    </GameProvider>
+  );
 }
 
 export default App;
